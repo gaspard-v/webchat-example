4dd57dfae3a90d67d2f327c9f9c78469587d2312
gaspard-v <60385905+gaspard-v@users.noreply.github.com>
2023-12-12 09:29:57 +0000

    Add .env.example file
    Add empty authentication.py file
    
    Add requirements.txt file

--- server/index.py
+++ server/index.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") 
 
 @app.route("/")
 def hello_world():
